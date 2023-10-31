e8af43e0954cf0c83f6e473528184b14e3dc1e64
kzzazzk <zsahraoui20@gmail.com>
2023-10-31 16:04:36 +0100

    Added on hover animations on MenuDrawer

--- portfolio/lib/widgets/menu_drawer_items/menu_drawer_ic_button.dart
+++ portfolio/lib/widgets/menu_drawer_items/menu_drawer_ic_button.dart
@@ -1,26 +1,80 @@
 import 'package:flutter/material.dart';
-import 'package:portfolio/utils/custom_icons.dart';
+import 'package:portfolio/utils/responsive.dart';
 
-class CustomIconButton extends StatelessWidget {
-  IconData iconData;
-  CustomIconButton({super.key, required this.iconData});
+class CustomIconButton extends StatefulWidget {
+  final IconData iconData;
+  const CustomIconButton({super.key, required this.iconData});
 
   @override
+  State<StatefulWidget> createState() => _CustomIconButtonState();
+}
+
+class _CustomIconButtonState extends State<CustomIconButton> {
+  bool isHovering = false;
+  bool isTapped = false;
+  @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Container(
-        decoration: BoxDecoration(
-          shape: BoxShape.circle,
-          border: Border.all(color: Colors.white, width: 1.5),
-        ),
-        child: IconButton(
-          onPressed: () => {},
-          icon: Icon(
-            iconData,
-            color: Colors.white,
-          ),
-        ),
-      ),
-    );
+    return ResponsiveWidget.isSmallScreen(context)
+        ? GestureDetector(
+            onTap: () {
+              setState(() {
+                isTapped = true;
+              });
+              Future.delayed(const Duration(milliseconds: 10), () {
+                setState(() {
+                  isTapped = false;
+                });
+              });
+            },
+            child: Center(
+              child: Container(
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  border: Border.all(color: Colors.white, width: 1.5),
+                ),
+                child: ClipRRect(
+                  borderRadius:
+                      BorderRadius.circular(50), // Adjust the radius as needed
+                  child: Container(
+                    color: isTapped
+                        ? Colors.red
+                        : Colors.blue, // Set the background color here
+                    child: IconButton(
+                      onPressed: () => {},
+                      icon: Icon(
+                        widget.iconData,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          )
+        : MouseRegion(
+            onEnter: (_) => setState(() => isHovering = true),
+            onExit: (_) => setState(() => isHovering = false),
+            child: Center(
+              child: Container(
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  border: Border.all(color: Colors.white, width: 1.5),
+                ),
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(50),
+                  child: Container(
+                    color: isHovering ? Colors.white10 : Colors.transparent,
+                    child: IconButton(
+                      onPressed: () => {},
+                      icon: Icon(
+                        widget.iconData,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          );
   }
 }
