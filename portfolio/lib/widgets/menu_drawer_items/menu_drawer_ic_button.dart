33c08d75e8f76243e1c469f23effd4e989a2fd1b
kzzazzk <zsahraoui20@gmail.com>
2023-11-01 22:49:27 +0100

    Fixed button padding

--- portfolio/lib/widgets/menu_drawer_items/menu_drawer_ic_button.dart
+++ portfolio/lib/widgets/menu_drawer_items/menu_drawer_ic_button.dart
@@ -22,20 +22,17 @@ class _CustomIconButtonState extends State<CustomIconButton> {
             shape: BoxShape.circle,
             border: Border.all(color: Colors.white, width: 1.5),
           ),
-          child: IconButton(
-            padding: EdgeInsets.zero,
+          child: TextButton(
             onPressed: () {},
             style: IconButton.styleFrom(
               foregroundColor: Colors.white,
               backgroundColor: Colors.transparent, // Text color
               shape: const CircleBorder(),
             ),
-            icon: Icon(
+            child: Icon(
               widget.iconData,
               color: Colors.white,
-            ),
-            alignment: Alignment
-                .center, // Center the icon both horizontally and vertically
+            ), // Center the icon both horizontally and vertically
           )),
     );
   }
