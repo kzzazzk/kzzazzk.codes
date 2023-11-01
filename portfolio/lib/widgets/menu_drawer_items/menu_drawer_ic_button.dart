87c0da0ae48e455e679ca48bd9b1d1fb95aec6f5
kzzazzk <zsahraoui20@gmail.com>
2023-11-01 22:01:52 +0100

    Fixed button padding

--- portfolio/lib/widgets/menu_drawer_items/menu_drawer_ic_button.dart
+++ portfolio/lib/widgets/menu_drawer_items/menu_drawer_ic_button.dart
@@ -14,22 +14,30 @@ class _CustomIconButtonState extends State<CustomIconButton> {
 
   @override
   Widget build(BuildContext context) {
-    return InkWell(
-      child: Container(
-        width: 40,
-        height: 40,
-        decoration: BoxDecoration(
-          shape: BoxShape.circle,
-          border: Border.all(color: Colors.white, width: 1.5),
+    return Container(
+      decoration: BoxDecoration(
+        shape: BoxShape.circle,
+        border: Border.all(
+          color: Colors.white, // border color
+          width: 2, // border width
         ),
-        child: TextButton(
-          onPressed: () {},
-          style: TextButton.styleFrom(
-            foregroundColor: Colors.white,
-            backgroundColor: Colors.transparent, // Text color
-            shape: const CircleBorder(),
+      ),
+      child: SizedBox.fromSize(
+        size: const Size(45, 45), // button width and height
+        child: ClipOval(
+          child: Material(
+            color: Colors.transparent, // button color
+            child: InkWell(
+              splashColor: Colors.transparent, // splash color
+              onTap: () {}, // button pressed
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Icon(widget.iconData, color: Colors.white), // icon
+                ],
+              ),
+            ),
           ),
-          child: Icon(widget.iconData),
         ),
       ),
     );
