a92c68cc2e39742c9688d621184a3e49c280af8e
kzzazzk <zsahraoui20@gmail.com>
2023-11-02 18:40:15 +0100

    Test for svg

--- portfolio/lib/screens/my_home_page.dart
+++ portfolio/lib/screens/my_home_page.dart
@@ -1,6 +1,7 @@
 import 'package:animate_gradient/animate_gradient.dart';
 import 'package:animated_text_kit/animated_text_kit.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 import 'package:portfolio/widgets/menu_drawer.dart';
 import 'package:portfolio/utils/responsive.dart';
 import 'package:portfolio/widgets/app_bar_content.dart';
@@ -41,9 +42,11 @@ class _MyHomePageState extends State<MyHomePage> {
                         onTap: () {
                           html.window.open('https://kzzazzk.codes', "_self");
                         },
-                        child: Image.asset(
-                          'assets/images/ZakaLogo.png',
-                          scale: 0.7,
+                        child: Center(
+                          child: SvgPicture.asset('assets/images/ZakaLogo.svg',
+                              width: 45,
+                              height: 45,
+                              semanticsLabel: 'PortfolioLogo Logo'),
                         ),
                       ),
                     ],
